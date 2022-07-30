f8fcd119115cf7911fcbe0dda107ea58ab0623a0
Martijn <12198037+Martijndewin@users.noreply.github.com>
2022-07-30 14:07:30 +0200

    feat(): add productBrand component - create productBrandCard component - show productBrandCard on home page

---
+++ src/components/ProductBrandCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-card>
+      <v-card-title>{{ name }}</v-card-title>
+      <v-card-text>
+        <v-chip
+          v-for="product in products"
+          @click="addToCard(product)"
+          :key="product.id"
+          >€{{ product.value }}</v-chip
+        >
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["name", "slug", "products"],
+  methods: {
+    addToCard(product) {
+      console.log(product);
+    }
+  }
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
     <h1>Products</h1>
+    <ProductBrandCardVue
+      v-for="product in products"
+      :key="product.slug"
+      :name="product.name"
+      :slug="product.slug"
+      :products="product.products"
+    />
   </div>
 </template>
 
@@ -8,12 +15,22 @@
 import axios from "axios";
 import store from "../store";
 
+import ProductBrandCardVue from "@/components/ProductBrandCard.vue";
+
 const baseUrl = "http://localhost:3000/products";
 export default {
   name: "Home",
-  components: {},
+  components: {
+    ProductBrandCardVue
+  },
+  computed: {
+    products() {
+      return store.state.products;
+    }
+  },
   async created() {
     try {
+      // get all products
       const res = await axios.get(baseUrl);
       store.commit("updateProducts", res.data);
     } catch (er) {

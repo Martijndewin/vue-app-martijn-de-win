f9591b65e90c534b3f14a4dde84390068babec35
Martijn <12198037+Martijndewin@users.noreply.github.com>
2022-07-30 13:56:47 +0200

    feat(): load product data into store - add state and mutation for loading product data - add created hook in home.vue to load products

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,24 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <h1>Products</h1>
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import axios from "axios";
+import store from "../store";
 
+const baseUrl = "http://localhost:3000/products";
 export default {
   name: "Home",
-  components: {}
+  components: {},
+  async created() {
+    try {
+      const res = await axios.get(baseUrl);
+      store.commit("updateProducts", res.data);
+    } catch (er) {
+      console.log(er);
+    }
+  }
 };
 </script>
